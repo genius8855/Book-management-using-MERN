2d449ad7c12fd6e8f9c29fbb2c1385081c2e69bc
genius8855 <aradhyy076@gmail.com>
2025-03-04 16:31:56 +0530

    build

--- backend/app.js
+++ backend/app.js
@@ -5,17 +5,25 @@ import { connect } from "mongoose";
 import routes from "./routes/bookRoutes.js"
 import cors from "cors";
 import connectDB from "./config/db.js";
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors());
+
+app.use(express.static(path.join(__dirname,'/frontend/dist')))
+
 dotenv.config();
 app.use(express.json());
 connectDB();
-app.use('/',routes);
+app.use('/api',routes);
 
 
-app.get('/', (req, res) => {
-    res.send("Hey Sahil!");
+app.get('/*', (req, res) => {
+   res.sendFile(path.join(__dirname,"/frontend/index.html"))
 })
 
 app.listen(PORT, () => {
